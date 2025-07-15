ee78f63fc5d768e78f6233277f544137743053b3
quantmharsh <srivastavah240@gmail.com>
2025-07-15 15:15:46 +0000

    (Feat):Added request validation for  publicKey , candidate name

--- src/app/api/vote/route.ts
+++ src/app/api/vote/route.ts
@@ -1,7 +1,8 @@
-import {ActionGetResponse, ACTIONS_CORS_HEADERS} from "@solana/actions"
+import {ActionGetResponse, ActionPostRequest, ACTIONS_CORS_HEADERS} from "@solana/actions"
 import * as anchor from '@coral-xyz/anchor';
 import { Voting } from "anchor/target/types/voting";
 import idl from '@/../anchor/target/idl/voting.json';
+import { PublicKey } from "@solana/web3.js";
 
 export const OPTIONS=GET;
 export async  function GET (request:Request){
@@ -40,7 +41,27 @@ export async function  POST (request:Request){
     /* Create a TypeScript interface to my Anchor voting program (with correct types), using this RPC connection, so I can easily build instructions, simulate or send transactions, and fetch accounts.*/
     const program : anchor.Program<Voting>= new anchor.Program(idl as Voting , {connection});
 
-    
+
+    const url = new URL(request.url);
+    const  vote = url.searchParams.get('candidate')as string;
+
+    if(vote!=="Crunchy" && vote!="Smooth")
+    {
+        return Response.json({error:'You voted for the wrong candidate'} , {status:400 ,  headers:ACTIONS_CORS_HEADERS});
+    }
+    //get body from request
+    const body:ActionPostRequest = await request.json();
+    let account: PublicKey;
+    try {
+        account = new PublicKey(body.account);
+    } catch (error) {
+        return new Response("Invalid account provided" , {
+            status:400 ,
+            headers:ACTIONS_CORS_HEADERS
+        })
+    }
+
+
 
     
 
